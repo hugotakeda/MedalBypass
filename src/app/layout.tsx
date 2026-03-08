4af908a7692a0553d5162e2dd9d5b16a1c041c3f
Hugo Takeda <57015662+hugotakeda@users.noreply.github.com>
2026-03-07 21:46:07 -0300

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9D4DPF1S6G"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9D4DPF1S6G');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.className} min-h-screen antialiased bg-[#09090b]`}>
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] pointer-events-none"></div>
         <div className="relative z-10 flex min-h-screen flex-col items-center justify-center p-4">
